26106e90675b55f0801a2af582c1676ef1f9f5c2
programacion <>
2020-10-23 12:55:52 +0200

    directivas inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private _mostrar: boolean = true;
+  private _listado: number[] = [34, 45, 43, 67, 86, 75, 41];
+  private _noIniciado: number;
   title = 'unoDirectivas2021Prof';
+
+    /**
+     * Getter mostrar
+     * @return {boolean }
+     */
+	public get mostrar(): boolean  {
+		return this._mostrar;
+	}
+
+    /**
+     * Setter mostrar
+     * @param {boolean } value
+     */
+	public set mostrar(value: boolean ) {
+		this._mostrar = value;
+	}
+
+    /**
+     * Getter listado
+     * @return {number[] }
+     */
+	public get listado(): number[]  {
+		return this._listado;
+	}
+
+    /**
+     * Setter listado
+     * @param {number[] } value
+     */
+	public set listado(value: number[] ) {
+		this._listado = value;
+	}
+
+    /**
+     * Getter noIniciado
+     * @return {number}
+     */
+	public get noIniciado(): number {
+		return this._noIniciado;
+	}
+
+    /**
+     * Setter noIniciado
+     * @param {number} value
+     */
+	public set noIniciado(value: number) {
+		this._noIniciado = value;
+	}
+
 }
